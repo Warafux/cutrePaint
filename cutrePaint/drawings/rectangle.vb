0667fb80be5e20f8789365c25936ee0dc6bb070b
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-10-16 06:28:17 +0200

    Bug with temporal rectangle
    The temporal rectangle didn't draw because coordenates were not fixed
    (they were only fixed at the creation of the shape)

--- cutrePaint/drawings/rectangle.vb
+++ cutrePaint/drawings/rectangle.vb
@@ -4,6 +4,19 @@
     Public Sub New(name As String, startPoint As Point, endPoint As Point, color As Color, visible As Boolean, width As Integer)
         MyBase.New(name, startPoint, endPoint, color, visible, width)
 
+    End Sub
+    Public Overrides Sub draw(paintEvent As PaintEventArgs)
+        If Not Me.visible Then
+            Return
+        End If
+
+        Dim graphics = paintEvent.Graphics
+
+
+        Dim startPointFix = Me.startPoint
+        Dim endPointFix = Me.endPoint
+
+        'Fix coordenates
         If ((Me.endPoint.X - Me.startPoint.X) < 0 Or (Me.endPoint.Y - Me.startPoint.Y) < 0) Then
             'Problem when startPoint and endPoint are not as desired (top-left corner and bottom-right corner)
             'Solution:http://www.vbforums.com/showthread.php?566861-Rectangle-negative-integers
@@ -13,18 +26,11 @@
                           Math.Abs(Me.startPoint.X - Me.endPoint.X),
                           Math.Abs(Me.startPoint.Y - Me.endPoint.Y))
 
-            Me.startPoint = rectangleArea.Location
-            Me.endPoint = New Point(rectangleArea.Location.X + rectangleArea.Width, rectangleArea.Location.Y + rectangleArea.Height)
+            startPointFix = rectangleArea.Location
+            endPointFix = New Point(rectangleArea.Location.X + rectangleArea.Width, rectangleArea.Location.Y + rectangleArea.Height)
 
         End If
-    End Sub
-    Public Overrides Sub draw(paintEvent As PaintEventArgs)
-        If Not Me.visible Then
-            Return
-        End If
 
-        Dim graphics = paintEvent.Graphics
-
-        graphics.DrawRectangle(New Pen(Me.color, Me.width), Me.startPoint.X, Me.startPoint.Y, Me.endPoint.X - Me.startPoint.X, Me.endPoint.Y - Me.startPoint.Y)
+        Graphics.DrawRectangle(New Pen(Me.color, Me.width), startPointFix.X, startPointFix.Y, endPointFix.X - startPointFix.X, endPointFix.Y - startPointFix.Y)
     End Sub
 End Class
