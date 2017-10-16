0e8eef30b55408b7024c128967f697236b086038
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-10-16 03:56:19 +0200

    Menu which shows actual drawings, needs fixes

---
+++ cutrePaint/menu.vb
@@ -0,0 +1,15 @@
+﻿Public Class menu
+    Private drawForm As Form
+    Private Sub menu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        drawForm = My.Forms.Form1
+    End Sub
+
+    Public Sub updateList(drawings As ArrayList)
+        listView_drawings.Items.Clear()
+        listView_drawings.Clear()
+
+        For Each drawing In drawings
+            listView_drawings.Items.Add(drawing.getName())
+        Next
+    End Sub
+End Class
